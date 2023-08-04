046b4589a2701be5819bff80a5b59981067df739
frank4177 <frankagu92@gail.com>
2023-08-04 13:11:58 +0100

    fixed button text

--- src/components/Form/LoanRequest.jsx
+++ src/components/Form/LoanRequest.jsx
@@ -37,7 +37,7 @@ const LoanRequestForm = ({ handleChange, payloadData, handleSubmit }) => {
         />
       </span>
 
-      <Button width="200px" />
+      <Button width="200px" text="Submit"/>
     </form>
   );
 };
